944420d9cfb03d7d26acfe986d7e5be615dd3996
Jamie McClymont <jamie@kwiius.com>
2020-05-20 23:42:13 +1200

    Download Packages in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd "$(mktemp -d)"
+
+wget https://mirror.fsmg.org.nz/kali/dists/kali-rolling/main/binary-amd64/Packages
+
 ruby parse-kali.rb Packages > kali-groups.json
 ruby parse-kali-descs.rb Packages > kali-descriptions.json
-# < kali-groups.json jq -r '.[] | .[]' | sort | uniq > kali-tools
