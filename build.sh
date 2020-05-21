3ddda5b734f7412bdba257c9f599c8ccb8e945d8
Jamie McClymont <jamie@kwiius.com>
2020-05-21 13:43:49 +1200

    Use gzipped Packages

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 packages="$(mktemp -u)"
 
-curl -Ss --fail https://mirror.fsmg.org.nz/kali/dists/kali-rolling/main/binary-amd64/Packages > "$packages"
+curl -Ss --fail https://mirror.fsmg.org.nz/kali/dists/kali-rolling/main/binary-amd64/Packages.gz | gunzip - > "$packages"
 
 ruby --encoding utf-8 parse-kali.rb < "$packages" > kali-groups.json
 ruby --encoding utf-8 parse-kali-descs.rb < "$packages" > kali-descriptions.json
